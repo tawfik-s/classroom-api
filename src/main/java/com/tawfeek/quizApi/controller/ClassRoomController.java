6e7763b9878095bc4b8fcb5b993b34f6a983d169
Tawfeek <t.shalash1@gmail.com>
2023-04-25 18:03:20 +0200

    finish implementing classroom endpoints

---
+++ src/main/java/com/tawfeek/quizApi/controller/ClassRoomController.java
@@ -0,0 +1,71 @@
+package com.tawfeek.quizApi.controller;
+
+
+import com.tawfeek.quizApi.model.classRoom.ClassRoomRequestDTO;
+import com.tawfeek.quizApi.model.classRoom.ClassRoomResponseDTO;
+import com.tawfeek.quizApi.model.classRoom.UserAddingToClassRoomStatusResponseDTO;
+import com.tawfeek.quizApi.model.user.UserResponseDTO;
+import com.tawfeek.quizApi.service.ClassRoomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/classrooms")
+public class ClassRoomController {
+
+    @Autowired
+    private ClassRoomService classRoomService;
+
+    @PostMapping("")
+    public ResponseEntity<ClassRoomResponseDTO> createClassRoom(@RequestBody ClassRoomRequestDTO classRoomRequestDTO) {
+        ClassRoomResponseDTO classRoomResponseDTO = classRoomService.createClassRoom(classRoomRequestDTO);
+        return new ResponseEntity<>(classRoomResponseDTO, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/owned")
+    public ResponseEntity<List<ClassRoomResponseDTO>> getOwnedClassRooms() {
+        List<ClassRoomResponseDTO> classRoomResponseDTOList = classRoomService.getOwnedClassRooms();
+        return new ResponseEntity<>(classRoomResponseDTOList, HttpStatus.OK);
+    }
+
+    @GetMapping("/member-of")
+    public ResponseEntity<List<ClassRoomResponseDTO>> getClassRoomsUserMemberOf() {
+        List<ClassRoomResponseDTO> classRoomResponseDTOList = classRoomService.getClassRoomsUserMemberOf();
+        return new ResponseEntity<>(classRoomResponseDTOList, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{classRoomId}")
+    public ResponseEntity<Void> deleteClassRoom(@PathVariable Long classRoomId) {
+        classRoomService.deleteClassRoom(classRoomId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/{classRoomId}/members")
+    public ResponseEntity<List<UserResponseDTO>> getClassRoomMembers(@PathVariable Long classRoomId) {
+        List<UserResponseDTO> userResponseDTOList = classRoomService.getClassRoomMembers(classRoomId);
+        return new ResponseEntity<>(userResponseDTOList, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{classRoomId}/members/{memberId}")
+    public ResponseEntity<Void> deleteClassRoomMember(@PathVariable Long classRoomId, @PathVariable Long memberId) {
+        classRoomService.deleteClassRoomMember(classRoomId, memberId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PostMapping("/{classRoomId}/members")
+    public ResponseEntity<UserAddingToClassRoomStatusResponseDTO> addMemberToClassRoom(@PathVariable Long classRoomId, @RequestBody String userEmail) {
+        UserAddingToClassRoomStatusResponseDTO userAddingToClassRoomStatusResponseDTO = classRoomService.addMemberToClassRoom(classRoomId, userEmail);
+        return new ResponseEntity<>(userAddingToClassRoomStatusResponseDTO, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/{classRoomId}/members/batch")
+    public ResponseEntity<List<UserAddingToClassRoomStatusResponseDTO>> addMembersToClassRoom(@PathVariable Long classRoomId, @RequestBody List<String> emails) {
+        List<UserAddingToClassRoomStatusResponseDTO> userAddingToClassRoomStatusResponseDTOList = classRoomService.addMembersToClassRoom(classRoomId, emails);
+        return new ResponseEntity<>(userAddingToClassRoomStatusResponseDTOList, HttpStatus.CREATED);
+    }
+
+}
--- src/main/java/com/tawfeek/quizApi/service/ClassRoomService.java
+++ src/main/java/com/tawfeek/quizApi/service/ClassRoomService.java
@@ -11,16 +11,18 @@ public interface ClassRoomService {
 
     public ClassRoomResponseDTO createClassRoom(ClassRoomRequestDTO classRoomRequestDTO);
 
-    public List<ClassRoomResponseDTO> getMyClassRooms();
+    public List<ClassRoomResponseDTO> getOwnedClassRooms();
+
+    public List<ClassRoomResponseDTO> getClassRoomsUserMemberOf();
 
     public void deleteClassRoom(Long classRoomId);
 
     public List<UserResponseDTO> getClassRoomMembers(Long classRoomId);
 
-    public UserAddingToClassRoomStatusResponseDTO deleteClassRoomMember(Long classRoomId, Long memberId);
+    public void deleteClassRoomMember(Long classRoomId, Long memberId);
 
-    public UserAddingToClassRoomStatusResponseDTO addMemberToClassRoom(Long classRoomId, String email);
+    public UserAddingToClassRoomStatusResponseDTO addMemberToClassRoom(Long classRoomId, String userEmail);
 
-    public List<UserAddingToClassRoomStatusResponseDTO> addMembersToClassRoom(List<String> emails);
+    public List<UserAddingToClassRoomStatusResponseDTO> addMembersToClassRoom(Long classRoomId,List<String> emails);
 
 }
