515f085f5c3c1552a2ca5b5db558fe7af1c793d2
Tawfeek <t.shalash1@gmail.com>
2023-05-24 15:29:21 +0300

    increase token time add cors to security

--- src/main/java/com/tawfeek/quizApi/security/SecurityConfiguration.java
+++ src/main/java/com/tawfeek/quizApi/security/SecurityConfiguration.java
@@ -23,7 +23,7 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+        http.cors().and()
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
