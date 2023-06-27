688d2344b1f5c20006b7cbd46a4c677bca259313
tawfeek shalash <t.shalash1@gmail.com>
2023-06-27 12:41:17 +0300

    update mappers and implement new ones

---
+++ src/main/java/com/tawfeek/quizApi/mapper/AdminQuizMapper.java
@@ -0,0 +1,16 @@
+package com.tawfeek.quizApi.mapper;
+
+import com.tawfeek.quizApi.entity.Question;
+import com.tawfeek.quizApi.entity.Quiz;
+import com.tawfeek.quizApi.model.quiz.AdminQuizWithQuestionRequestDTO;
+import com.tawfeek.quizApi.model.quiz.AdminQuizWithQuestionsResponseDTO;
+
+import java.util.List;
+
+public interface AdminQuizMapper {
+
+  public AdminQuizWithQuestionsResponseDTO toDTO(Quiz quiz);
+
+  public Quiz toEntity(
+          AdminQuizWithQuestionRequestDTO quizCreationWithQuestionsDTO, List<Question> questions);
+}
---
+++ src/main/java/com/tawfeek/quizApi/mapper/Impl/AdminQuizMapperImpl.java
@@ -0,0 +1,43 @@
+package com.tawfeek.quizApi.mapper.Impl;
+
+import com.tawfeek.quizApi.entity.Question;
+import com.tawfeek.quizApi.entity.Quiz;
+import com.tawfeek.quizApi.mapper.AdminQuestionMapper;
+import com.tawfeek.quizApi.mapper.AdminQuizMapper;
+import com.tawfeek.quizApi.model.quiz.AdminQuizWithQuestionRequestDTO;
+import com.tawfeek.quizApi.model.quiz.AdminQuizWithQuestionsResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class AdminQuizMapperImpl implements AdminQuizMapper {
+
+  @Autowired AdminQuestionMapper adminQuestionMapper;
+
+  @Override
+  public AdminQuizWithQuestionsResponseDTO toDTO(Quiz quiz) {
+    AdminQuizWithQuestionsResponseDTO quizResponse =
+        new AdminQuizWithQuestionsResponseDTO();
+    quizResponse.setName(quiz.getName());
+    quizResponse.setDuration(quiz.getDuration());
+    quizResponse.setId(quiz.getId());
+    quizResponse.setCloseDate(quiz.getCloseDate());
+    quizResponse.setCreationDateTime(quiz.getCreationDateTime());
+    quizResponse.setQuestions(
+        quiz.getQuestions().stream().map(question -> adminQuestionMapper.toDTO(question)).toList());
+    return null;
+  }
+
+  @Override
+  public Quiz toEntity(AdminQuizWithQuestionRequestDTO quizRequest, List<Question> questions) {
+    Quiz quiz = new Quiz();
+    quiz.setName(quizRequest.getName());
+    quiz.setDuration(quizRequest.getDuration());
+    quiz.setCreationDateTime(quizRequest.getCreationDateTime());
+    quiz.setCloseDate(quizRequest.getCloseDate());
+    quiz.setQuestions(questions);
+    return quiz;
+  }
+}
