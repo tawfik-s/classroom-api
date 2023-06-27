63269e279e50dbb23214f30cd18f0f48cbe7e5b8
tawfeek shalash <t.shalash1@gmail.com>
2023-06-27 13:53:26 +0300

    rename user to member

--- src/main/java/com/tawfeek/quizApi/controller/MemberQuizController.java
+++ src/main/java/com/tawfeek/quizApi/controller/MemberQuizController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/classrooms")
-public class UserQuizController {
+public class MemberQuizController {
 
     // Take quiz
     @PostMapping("/quizzes/{quizId}/take")
